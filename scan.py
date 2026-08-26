3dbd1f3be7be75ace0ccae07e6f6f7944c78e84c
xiao <rehuyaoshou@gmail.com>
2026-08-25 22:46:29 -0600

    feat: populate module behavior surface
    before you get carried away topo-sorting TU edges, you need to
    understand what modules are trying to do... at all. that means
    identifying then categorizing module declarations along behavioral
    boundaries, so your future parser can meaningfully interpret what an
    edge actually is.
    
    also no, this isn't difficult at all: the complete domain is three
    words long (import, export, module) and I keep a tight leash on what I
    consider valid representation. yes, your foreign code will hard error
    out. and, no, forbidding annoying edge cases means I'm opinionated,
    not lazy. remember that, folks.

--- scan.py
+++ scan.py
@@ -4,7 +4,46 @@ import subprocess
 
 CXX = "ez80-clang"
 
+# yes, I used box art simply to dodge:
+# 
+#   # <line> "<file>" [flags]
+#
+# the double pound bothered me that much.
+
+# *-----------------------------*
+# |  # <line> "<file>" [flags]  |
+# *-----------------------------*
 LINEMARK = re.compile(r'^#\s+\d+\s+"([^"]*)"')
+
+# *--------------------------------*
+# |  export module <name[:part]>;  |
+# *--------------------------------*
+IFACE_UNIT = re.compile(r"^\s*export\s+module\s+([\w.]+(?::[\w.]+)?)\s*;")
+# *---------------------------*
+# |  module <name>[<:part>];  |
+# *---------------------------*
+IMPL_UNIT = re.compile(r"^\s*module\s+([\w.]+)(:[\w.]+)?\s*;")
+# *---------------------------------------*
+# |  [export] import [:]<module[:part]>;  |
+# *---------------------------------------*
+IMPORT_DECL = re.compile(r"^\s*(export\s+)?import\s+(:?[\w.]+(?::[\w.]+)?)\s*;")
+
+# *------------------------*
+# |  [export] import <...  |
+# |  [export] import "...  |
+# *------------------------*
+HEADER_IMPORT = re.compile(r'^\s*(?:export\s+)?import\s*[<"]')
+# *---------------------*
+# |  module;            |
+# |  module : private;  |
+# *---------------------*
+FRAGMENT = re.compile(r"^\s*module\s*(?::\s*private)?\s*;")
+
+# *------------------------------------------------*
+# |  [export] (module|import) <module-ish text>;   |
+# *------------------------------------------------*
+UNRECOGNIZED = re.compile(r"^\s*(?:export\s+)?(?:module|import)(?!\w)\s*[\w.\s:]*;")
+
 
 def _preprocess(source_file, flags):
     cmd = [CXX, "-E", *flags, source_file]
@@ -12,7 +51,12 @@ def _preprocess(source_file, flags):
     proc = subprocess.run(cmd, capture_output=True, text=True)
 
     if proc.returncode != 0:
-        raise RuntimeError(f'moddeps: {shlex.join(cmd)}\n{proc.stderr}')
+        raise RuntimeError(
+            f"moddeps: {shlex.join(cmd)}\n"
+            "\n"
+            f"{proc.stderr}\n"
+            "\n"
+        )
 
     return proc.stdout
 
@@ -24,13 +68,65 @@ def _source_owned_lines(source_file, preprocessed):
         if marker := LINEMARK.match(line):
             current_file = marker.group(1)
             continue
-            
+
         if current_file == source_file:
             yield line
 
 
-if __name__ == "__main__":
-    file = "strata/ok.cpp"
+def parse_unit(source_file, preprocessed):
+    for line in _source_owned_lines(source_file, preprocessed):
 
-    for line in _source_owned_lines(file, _preprocess(file, [])):
-        print(line)
+        if HEADER_IMPORT.match(line):
+            raise RuntimeError(
+                f"moddeps: {source_file}\n"
+                f"  { line.strip() }\n"
+                "\n"
+                "header units aren't currently possible! please use a global module fragment:\n"
+                "\n"
+                "  module;\n"
+                "\n"
+                "  #include <header1>\n"
+                "  #include <header2>\n"
+                "\n"
+                '  #include "header3"\n'
+                '  #include "header4"\n'
+                "\n"
+                "  export module my_iface;\n"
+                "\n"
+            )
+
+        if IFACE_UNIT.match(line):
+            print(f"[IFACE_UNIT] {line}")
+            continue
+
+        if IMPL_UNIT.match(line):
+            print(f"[IMPL_UNIT] {line}")
+            continue
+
+        if IMPORT_DECL.match(line):
+            print(f"[IMPORT_DECL] {line}")
+            continue
+
+        if UNRECOGNIZED.match(line) and not FRAGMENT.match(line):
+            raise RuntimeError(
+                f"moddeps: {source_file}\n"
+                "\n"
+                "this module declaration is unrecognized:\n"
+                "\n"
+                f"  { line.strip() }\n"
+                "\n"
+                "hint: use canonical spacing.\n"
+                "\n"
+                "BAD:                          GOOD:                         \n"
+                "  import:iface;                 import :iface;              \n"
+                "  import: iface;                                            \n"
+                "  import : iface;                                           \n"
+                "\n"
+                "don't be a freak with your linter ;)\n"
+                "\n"
+            )
+
+
+if __name__ == "__main__":
+    source = "strata/zoo.cpp"
+    parse_unit(source, _preprocess(source, []))
---
+++ strata/zoo.cpp
@@ -0,0 +1,20 @@
+// what kind of sicko lets farm animals into a zoo??
+
+module;
+export module moo;
+
+import oink;
+import :baa;
+
+export import :baa;
+module :private;
+
+module moo;
+module moo:baa;
+export module moo:baa;
+
+// invalid (uncomment):
+//
+// import <coo>;
+// module cluck neigh;
+// import:honk;
